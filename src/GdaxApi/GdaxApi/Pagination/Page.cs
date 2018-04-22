0c8ac17a9422999c2330adca6f99496fb2b54d3d
PiezPiedPy <githubpie@maxautodesign.com>
2018-04-22 17:01:02 +0100

    Pages: Make pages Enumerable
    Allows pages to be iterated through with a foreach loop

--- src/GdaxApi/GdaxApi/Pagination/Page.cs
+++ src/GdaxApi/GdaxApi/Pagination/Page.cs
@@ -1,11 +1,57 @@
-﻿namespace GdaxApi.Pagination
+﻿using System;
+using System.Collections;
+
+namespace GdaxApi.Pagination
 {
-    public class Page<T, U>
+    public class Page<T, U>: IEnumerable
     {
         public T[] Items { get; internal set; }
 
         public U Before { get; internal set; }
 
         public U After { get; internal set; }
+
+
+        //private enumerator class
+        private class PageEnumerator: IEnumerator
+        {
+            private T[] Items;
+            private int position = -1;
+
+            //constructor
+            public PageEnumerator(T[] list)
+            {
+                Items = list;
+            }
+
+            public bool MoveNext()
+            {
+                position++;
+                return (position < Items.Length);
+            }
+
+            public void Reset() { position = -1; }
+
+            public object Current
+            {
+                get
+                {
+                    try
+                    {
+                        return Items[position];
+                    }
+
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new PageEnumerator(Items);
+        }
     }
 }
